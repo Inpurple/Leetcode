68ab41c0c33e69867513a618bbf06ac27aed2411
Cherry Zhou <42965594+Inpurple@users.noreply.github.com>
2019-05-15 12:19:38 +0800

    Create Solution_sort方法.py

---
+++ 215. Kth Largest Element in an Array/Solution_sort方法.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        nums.sort()
+        return nums[len(nums)-k]
