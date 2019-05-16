3c5b99a442db4dc305cf6afddaf50cd5121cb25d
Cherry Zhou <42965594+Inpurple@users.noreply.github.com>
2019-05-16 15:53:08 +0800

    Create Solution_快速排序.py

---
+++ 912. Sort an Array/Solution_快速排序.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def sortArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+
+        self.quicksort_recur(0,len(nums)-1,nums)
+        return nums
+    
+    
+    def partition(self,left,right,array):
+        key=left
+        while left<right:
+            if array[left]>array[key] and array[right]<array[key]:
+                array[left],array[right]=array[right],array[left]
+                #left=left+1
+                #right=right-1
+            elif array[right]>=array[key]:#等号容易忘掉
+                right=right-1
+            elif array[left]<=array[key]:
+                left=left+1
+        array[key],array[right]=array[right],array[key]
+        return right
+    
+    def quicksort_recur(self,left,right,array):
+        if left>=right:
+            return 
+        else:
+            mid=self.partition(left,right,array)
+            self.quicksort_recur(left,mid-1,array)
+            self.quicksort_recur(mid+1,right,array)
