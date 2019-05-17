7ace17ec39dcdccfd5af3707688f272b36d2ee56
Cherry Zhou <42965594+Inpurple@users.noreply.github.com>
2019-05-17 15:03:10 +0800

    Create Solution_选择排序_reversed()方法.py

---
+++ 969. Pancake Sorting/Solution_选择排序_reversed()方法.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def pancakeSort(self, A):
+        """
+        :type A: List[int]
+        :rtype: List[int]
+        
+        反转方法一：反转i+1：A[0:i+1]=A[i::-1]---参数复杂：
+        反转方法二：反转3：a[0:3] = list(reversed(a[0:3]))---reversed（sequence） - >反转迭代器的序列值
+返回反向迭代器
+        算法利用选择排序的思想
+        
+        """
+        
+        r=[]
+        for i in range(len(A)-1,0,-1):
+            m=float("-inf")
+            for j in range(0,i+1):
+                if A[j]>m:
+                    m=A[j]
+                    m_i=j
+            if m_i==0:
+                r.append(i+1)
+                A[0:i+1]=list(reversed(A[0:i+1]))
+            elif m_i==i:
+                pass
+            else:
+                r.append(m_i+1)
+                A[0:m_i+1]=list(reversed(A[0:m_i+1]))
+                r.append(i+1)
+                A[0:i+1]=list(reversed(A[0:i+1]))
+        
+        return r
