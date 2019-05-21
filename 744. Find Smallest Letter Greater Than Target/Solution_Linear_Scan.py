9267b27d41be71ab5a20eba42521487d4a724fcb
Cherry Zhou <42965594+Inpurple@users.noreply.github.com>
2019-05-21 14:34:47 +0800

    Create Solution_Linear_Scan.py

---
+++ 744. Find Smallest Letter Greater Than Target/Solution_Linear_Scan.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def nextGreatestLetter(self, letters, target):
+        """
+        :type letters: List[str]
+        :type target: str
+        :rtype: str
+        """
+        for letter in letters:
+            if letter>target:
+                return letter
+        
+        return letters[0]
