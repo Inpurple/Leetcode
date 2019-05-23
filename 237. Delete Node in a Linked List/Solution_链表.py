f88848788ad5a7c081f904c3444674b85fb2b94e
Cherry Zhou <42965594+Inpurple@users.noreply.github.com>
2019-05-23 18:06:11 +0800

    Create Solution_链表.py

---
+++ 237. Delete Node in a Linked List/Solution_链表.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        先替换，再删除
+        """
+        node.val=node.next.val
+        node.next=node.next.next
