e169b1ca2371572755f716ace3ff3a241d855040
Cherry Zhou <42965594+Inpurple@users.noreply.github.com>
2019-06-26 17:49:39 +0800

    Create Solution_大神的代码.py

---
+++ 621. Task Scheduler/Solution_大神的代码.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def leastInterval(self, tasks, n):
+        """
+        :type tasks: List[str]
+        :type n: int
+        :rtype: int
+        """
+        length = len(tasks)
+        if length <= 1:
+            return length
+        
+        # 用于记录每个任务出现的次数
+        task_map = dict()
+        for task in tasks:
+            task_map[task] = task_map.get(task, 0) + 1
+        # 按任务出现的次数从大到小排序
+        task_sort = sorted(task_map.items(), key=lambda x: x[1], reverse=True)
+        
+        # 出现最多次任务的次数
+        max_task_count = task_sort[0][1]
+        # 至少需要的最短时间
+        res = (max_task_count - 1) * (n + 1)
+        
+        for sort in task_sort:
+            if sort[1] == max_task_count:
+                res += 1
+        
+        # 如果结果比任务数量少，则返回总任务数
+        return res if res >= length else length
+
+作者：jalan
+链接：https://leetcode-cn.com/problems/two-sum/solution/python-xiang-jie-by-jalan/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
