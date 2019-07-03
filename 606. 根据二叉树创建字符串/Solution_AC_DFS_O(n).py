dbafa18002aedebd5d43e417dec61a727176ecc1
Cherry Zhou <42965594+Inpurple@users.noreply.github.com>
2019-07-03 15:22:17 +0800

    Update Solution_AC_DFS_O(n).py

--- 606. 根据二叉树创建字符串/Solution_AC_DFS_O(n).py
+++ 606. 根据二叉树创建字符串/Solution_AC_DFS_O(n).py
@@ -8,21 +8,15 @@
 class Solution:
     def tree2str(self, t: TreeNode) -> str:
 
-        def dfs(t):
-            if not t:
-                return ""
-            if not t.right and not t.left:
-                return str(t.val)
-            elif t.right and t.left:
-                return str(t.val)+"("+dfs(t.left)+")"+"("+dfs(t.right)+")"
-            elif t.left and not t.right:
-                return str(t.val)+"("+dfs(t.left)+")"  
-            elif not t.left and t.right:
-                return str(t.val)+"()"+"("+dfs(t.right)+")"
-
-
-        
-        return dfs(t)
-    
+        if not t:
+            return ""
+        if not t.right and not t.left:
+            return str(t.val)
+        elif t.right and t.left:
+            return str(t.val)+"("+self.tree2str(t.left)+")"+"("+self.tree2str(t.right)+")"
+        elif t.left and not t.right:
+            return str(t.val)+"("+self.tree2str(t.left)+")"  
+        elif not t.left and t.right:
+            return str(t.val)+"()"+"("+self.tree2str(t.right)+")"
 
     
