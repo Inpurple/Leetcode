e794360e14e5a35c16a3d7f10d37ca71a7585612
Cherry Zhou <42965594+Inpurple@users.noreply.github.com>
2019-07-05 15:36:18 +0800

    Create Solution_DFS_自下而上.py

---
+++ 104. Maximum Depth of Binary Tree/Solution_DFS_自下而上.py
@@ -0,0 +1,30 @@
+
+# Definition for a binary tree node.
+
+# class TreeNode(object):
+
+#     def __init__(self, x):
+
+#         self.val = x
+
+#         self.left = None
+
+#         self.right = None
+
+
+
+class Solution(object):
+
+    def maxDepth(self, root):#    自下而上法
+
+        """
+
+        :type root: TreeNode
+
+        :rtype: int
+
+        """
+        if not root:
+            return 0
+        else:
+            return max(self.maxDepth(root.left),self.maxDepth(root.right))+1
