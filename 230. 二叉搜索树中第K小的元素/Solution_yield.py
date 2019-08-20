de20e2d3bbc84b5a89d415d7ac475b2e502d742b
Cherry Zhou <42965594+Inpurple@users.noreply.github.com>
2019-08-20 13:13:21 +0800

    Create Solution_yield.py

---
+++ 230. 二叉搜索树中第K小的元素/Solution_yield.py
@@ -0,0 +1,12 @@
+class Solution:
+    def mid_order(self, root):
+        if not root: return
+        yield from self.mid_order(root.left)
+        yield root.val
+        yield from self.mid_order(root.right)
+        
+    def kthSmallest(self, root: TreeNode, k: int) -> int:
+        gen = self.mid_order(root)
+        for _ in range(k - 1):
+            next(gen)
+        return next(gen)
